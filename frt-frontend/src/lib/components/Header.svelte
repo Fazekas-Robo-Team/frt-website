89bb586e7b8836aa1116c9866e2040382fe78967
Daniel Gergely <vargaking2005@gmail.com>
2023-02-03 21:08:55 +0100

    img url fix

--- frt-frontend/src/lib/components/Header.svelte
+++ frt-frontend/src/lib/components/Header.svelte
@@ -2,7 +2,6 @@
 	// @ts-nocheck
 	import { onMount } from 'svelte';
 
-	import current_article_index_pic from '$lib/images/robot.jpg';
 </script>
 
 <header class="w-screen h-screen border-b-black border-b-2 bg-light-blue flex flex-col md:flex-row justify-center md:justify-between items-center px-20 mb-64">
@@ -14,7 +13,7 @@
 	<a class="group mx-4 w-full md:w-3/6" href="/"><div class="flex items-center flex-col w-full h-full">
 		<div class="bg-white w-4/5 md:w-3/5 text-lg md:text-xl lg:text-2xl font-semibold border-black border-2 -mb-4 z-10 p-1 md:p-2 -mr-24 group-hover:translate-x-5px group-hover:translate-y-5px">Indul a WRO 2023-as szezonja!</div>
 		<div class="border-black border-2 bg-white flex justify-center items-center drop-shadow-xl-purple-br group-hover:drop-shadow-md-purple-br group-hover:translate-x-5px group-hover:translate-y-5px">
-			<img class="m-10 w-4/5 border-black border-2" src={current_article_index_pic} alt="" />
+			<img class="m-10 w-4/5 border-black border-2" src="/blog_images/robot.jpg" alt="" />
 		</div>
 	</div></a>
 </header>
