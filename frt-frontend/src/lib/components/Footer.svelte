ff36f30b531a7719a3c1231472352ea651683f95
Daniel Gergely <vargaking2005@gmail.com>
2023-02-04 19:05:10 +0100

    blog redesigns

--- frt-frontend/src/lib/components/Footer.svelte
+++ frt-frontend/src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="bg-white flex justify-center">
+<footer class="bg-white flex justify-center border-black border-t-2">
     <h5 class="text-black m-3">© 2023 - Fazekas RoboTeam</h5>
 </footer>
--- frt-frontend/src/routes/blog/+page.svelte
+++ frt-frontend/src/routes/blog/+page.svelte
@@ -2,7 +2,7 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Date from '$lib/images/date.svg';
 	import Pfp from '$lib/images/pfp.svg';
-	import ArticleCard from '$lib/components/Article_card.svelte';
+	import ArticleCard from '$lib/components/Article_card_blogpage.svelte';
 
 	export let data: any;
 </script>
@@ -12,7 +12,7 @@
 	<meta name="description" content="FRT Blog" />
 </svelte:head>
 
-<div class="px-8 mt-16 border-b-2 border-black">
+<div class="px-8 mt-24 mb-8 w-screen grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 	{#each data.posts as article}
 	<ArticleCard {article} />
 	{/each}
