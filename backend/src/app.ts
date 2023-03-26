fbdbafb09f0e66cd29d3722bb9a1f5a7f845633a
Daniel Gergely <vargaking2005@gmail.com>
2023-03-26 20:55:03 +0200

    cors fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -84,7 +84,6 @@ app.use(cookieParser());
 app.use(cors(
     {
         origin: ["http://localhost:5173", "http://localhost:5000", "http://vargaking.me:5173", "http://vargaking.me:5000"],
-        credentials: true,
     }
 ));
 app.use(express.json());
