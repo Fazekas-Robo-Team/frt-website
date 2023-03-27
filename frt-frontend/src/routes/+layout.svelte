ed2e8e2914c1de93f3374432a7d41ee5844452e6
Daniel Gergely <vargaking2005@gmail.com>
2023-03-27 20:59:48 +0200

    scroll issue fix

--- frt-frontend/src/routes/+layout.svelte
+++ frt-frontend/src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
 	import './styles.css';
 	import '../app.scss';
+	import { onMount } from 'svelte';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+
+	let root: any;
+
+	onMount(() => {
+		root = document.getElementsByTagName('html')[0];
+		root.classList.add('smoothscroll');
+	});
+
+	beforeNavigate(() => {
+		root.classList.remove('smoothscroll');
+	});
+
+	afterNavigate(() => {
+		root.classList.add('smoothscroll');
+	});
 </script>
 
 <slot></slot>
