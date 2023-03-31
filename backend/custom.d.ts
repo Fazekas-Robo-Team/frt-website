d376f43a48eb19ebdac918e9a928c6e0a6c0f8f4
Daniel Gergely <vargaking2005@gmail.com>
2023-03-31 11:12:00 +0200

    removed backend from repo

--- backend/custom.d.ts
+++
@@ -1,7 +0,0 @@
-// custom.d.ts
-declare namespace Express {
-    export interface Request {
-        userId?: number;
-        session: session.Session & Partial<session.SessionData> & { token?: string };
-    }
-}
--- backend/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import AuthController from '../controllers/authController';
-
-const router = Router();
-
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
-
-export default router;
