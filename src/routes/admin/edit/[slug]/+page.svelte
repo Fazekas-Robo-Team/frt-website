a9b54f721ffb0419b6afac5bff58e9cf42a457ff
Daniel Gergely <vargaking2005@gmail.com>
2023-04-03 22:15:24 +0200

    article editing improvements

--- src/routes/admin/edit/[slug]/+page.svelte
+++ src/routes/admin/edit/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import { PUBLIC_BACKEND_URL } from "$env/static/public";
+    import { PUBLIC_BACKEND_URL, PUBLIC_IMAGE_URL } from "$env/static/public";
     import MultipleImageUpload from '$lib/components/Multiple_image_upload.svelte';
+	import Image from "$lib/markdown/image.svelte";
 	import { loading, modal } from "../../../../stores";
 
     export let data: any;
@@ -59,31 +60,43 @@
 
 <!-- new post form -->
 
-<form id="form" class="flex flex-col w-1/2 mx-auto my-8" on:submit|preventDefault={submit}>
+<form id="form" class="flex flex-row w-3/4 mx-auto my-8" on:submit|preventDefault={submit}>
+    <div class="flex flex-col w-2/3">
+        <label for="title" class="text-lg font-semibold text-white">Title</label>
 
-    <label for="title" class="text-lg font-semibold text-white">Title</label>
-
-    <input disabled type="text" autocomplete="off" id="title" class="border-2 border-gray-300 p-2 rounded my-2" bind:value={title} />
-
-    <label for="description" class="text-lg font-semibold text-white">Description</label>
-
-    <input type="text" autocomplete="off" id="description" class="border-2 border-gray-300 p-2 rounded my-2" bind:value={description} />
-
-    <label for="content" class="text-lg font-semibold text-white">Content</label>
-
-    <textarea id="content" autocomplete="off" class="border-2 border-gray-300 p-2 rounded my-2 h-64" bind:value={content} />
-
-    <label for="category" class="text-lg font-semibold text-white">Category</label>
-
-    <input disabled type="text" autocomplete="off" id="category" class="border-2 border-gray-300 p-2 rounded my-2" bind:value={category} />
-
-    <MultipleImageUpload />
-
-    <!-- index image -->
-
-    <label for="index" class="text-lg font-semibold text-white">Index Image</label>
-
-    <input type="file" id="index" class="border-2 border-white bg-white p-2 rounded my-2" />
-
-    <button type="submit" class="bg-emerald-600 text-white p-2 rounded my-2 hover:brightness-75 transition-all">Save</button>
+        <input disabled type="text" autocomplete="off" id="title" class="border-2 border-gray-300 p-2 rounded my-2" bind:value={title} />
+    
+        <label for="description" class="text-lg font-semibold text-white">Description</label>
+    
+        <input type="text" autocomplete="off" id="description" class="border-2 border-gray-300 p-2 rounded my-2" bind:value={description} />
+    
+        <label for="content" class="text-lg font-semibold text-white">Content</label>
+    
+        <textarea id="content" autocomplete="off" class="border-2 border-gray-300 p-2 rounded my-2 h-64" bind:value={content} />
+    
+        <label for="category" class="text-lg font-semibold text-white">Category</label>
+    
+        <input disabled type="text" autocomplete="off" id="category" class="border-2 border-gray-300 p-2 rounded my-2" bind:value={category} />
+    
+        <MultipleImageUpload />
+    
+        <!-- index image -->
+    
+        <label for="index" class="text-lg font-semibold text-white">Index Image (uneditable)</label>
+    
+        <input disabled type="file" id="index" class="border-2 border-white bg-white p-2 rounded my-2" />
+    
+        <button type="submit" class="bg-emerald-600 text-white p-2 rounded my-2 hover:brightness-75 transition-all">Save</button>
+    </div>
+    <div class="w-1/3">
+        <h3 class="text-lg font-semibold text-white text-center">Already uploaded images</h3>
+        <div class="grid grid-cols-2 gap-2 w-full">
+            {#each data.imageUrls as imageUrl, i}
+            <div>
+                <img src={`${PUBLIC_IMAGE_URL}/${imageUrl}`} alt="Preview" class="w-32 h-32 m-2 object-cover" />
+                <span class="text-white">{data.imageNames[i]}</span>
+            </div>
+            {/each}
+        </div>
+    </div>
 </form>
