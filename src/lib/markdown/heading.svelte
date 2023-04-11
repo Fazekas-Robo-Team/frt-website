f6f013dac5ab7005f0096e7e171d91543193de52
Daniel Gergely <vargaking2005@gmail.com>
2023-04-11 14:49:10 +0200

    Update heading.svelte

--- src/lib/markdown/heading.svelte
+++ src/lib/markdown/heading.svelte
@@ -4,15 +4,9 @@
 </script>
 
 {#if depth === 1}
-	<h1 class="text-4xl font-bold mb-4">{text}</h1>
+	<h2 class="text-4xl font-bold mb-4">{text}</h2>
 {:else if depth === 2}
-	<h2 class="text-3xl font-bold mb-4">{text}</h2>
+	<h3 class="text-3xl font-bold mb-4">{text}</h3>
 {:else if depth === 3}
-	<h3 class="text-2xl font-bold mb-4">{text}</h3>
-{:else if depth === 4}
-	<h4 class="text-xl font-bold mb-4">{text}</h4>
-{:else if depth === 5}
-	<h5 class="text-lg font-bold mb-4">{text}</h5>
-{:else if depth === 6}
-	<h6 class="text-base font-bold mb-4">{text}</h6>
+	<h4 class="text-2xl font-bold mb-4">{text}</h4>
 {/if}
