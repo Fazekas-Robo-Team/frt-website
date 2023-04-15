318fb95ded1b019e4f003ea31c3a0b789a5a8ab1
Daniel Gergely <vargaking2005@gmail.com>
2023-04-15 22:29:57 +0200

    new post

---
+++ src/routes/admin/new/+page.server.ts
@@ -0,0 +1,36 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load = (async ({ locals: { supabase, getSession }}) => {
+    // create a new article with title: "Draft by {user}"
+
+    // redirect to the edit page for the new article
+
+    // get the user id from the session
+
+    const session = await getSession();
+
+    
+
+    if (!session) {
+        return fail(500, "You must be logged in to create a new article");
+    }
+
+    // get the user full_name from the profiles table
+    const { data: profile } = await supabase.from('profiles').select('full_name').eq('id', session.user.id);
+
+
+    const { data:article, error } = await supabase.from('articles').insert({
+        title: `Draft by ${profile?.[0].full_name}`,
+        author_id: session.user.id,
+    }).select();
+
+    if (error) {
+        return fail(500, error);
+    }
+
+    console.log(article, "article");
+
+    throw redirect(303, `/admin/edit/${article?.[0].id}`);
+
+}) satisfies PageServerLoad;
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -4,9 +4,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	const session = await getSession();
-
-	console.log(session)
-
+	
     // if the user is already logged in return them to the account page
 	if (session) {
 		throw redirect(303, '/admin');
