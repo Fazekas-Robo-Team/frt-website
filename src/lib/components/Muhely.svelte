7e7b586b2814c21c2f1d8a86dc39f1b807e065fc
Daniel Gergely <vargaking2005@gmail.com>
2023-04-17 23:27:34 +0200

    updated static content

--- src/lib/components/Muhely.svelte
+++ src/lib/components/Muhely.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-	//TODO: replace with better image
 	import Muhely from '$lib/images/muhely.webp';
 </script>
 
 <div class="w-screen h-fit md:h-screen bg-light-orange border-black border-y-2 flex flex-col items-center px-4 py-24 mb-64" id="muhely">
 	<div class="bg-medium-orange px-3 pb-4 h-2 mt-4 mb-8 ml-4"><h2 class="text-black text-4xl font-semibold -mt-6 -ml-2">Műhely</h2></div>
 	<div class="w-full h-full flex flex-col-reverse lg:flex-row justify-center items-center md:px-10">
-		<img class="w-3/5 lg:w-1/3 border-black border-2 drop-shadow-xl-black-br mt-8 mx-10 lg:mx-15" src={Muhely} alt="" />
+		<img class="w-3/5 aspect-4/3 object-cover lg:w-1/3 border-black border-2 drop-shadow-xl-black-br mt-8 mx-10 lg:mx-15" src={Muhely} alt="A FRT műhelye" />
 		<p class="w-4/5 lg:w-1/3 text-2xl font-semibold mx-10 lg:mx-15">
-			A Fazekas aulájának hátuljában húzodik meg a 115/A terem, amely már évek óta a FRT főhadibázisaként operál. Az évek alatt sikerült egy rendkívül
-			fejlett infrastruktúrát kialakítani modern laptopokkal és eszközökkel, de persze mindig van hely az olyan klasszikusoknak, mint amilyen például
-			a Mindstorms RCX.
+			A Fazekas RoboTeam bázisa az iskola aulájában található 115/A terem. Az elmúlt húsz év patronálásainak köszönhetően mára egy
+			jól felszerelt robotika műhely áll diákjaink rendelkezésére. Tehetséggondozó programunkban Lego Mindstorms EV3, REV Robotics kit és Arduino
+			eszközökkel dolgozunk.
 		</p>
 	</div>
 </div>
