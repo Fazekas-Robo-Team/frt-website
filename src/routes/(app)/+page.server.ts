58c15c7cfb9fee11c9ee2c559ecf4e23c1375bfb
Daniel Gergely <vargaking2005@gmail.com>
2023-04-21 22:42:59 +0200

    blog homepage fixes

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -2,8 +2,11 @@ import { fail } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ locals: { supabase } }) => {
-    const { data: articles, error } = await supabase.from('articles').select('*, profiles(full_name)').order('created_at', { ascending: true });
+    //const { data: articles, error } = await supabase.from('articles').select('*, profiles(full_name)').order('created_at', { ascending: true });
 
+    // get the 3 latest, published articles
+    const { data: articles, error } = await supabase.from('articles').select('*, profiles(full_name)').eq('published', true).order('created_at', { ascending: false }).limit(3);
+    
     // get featured post
     const { data: featured } = await supabase.from('articles').select('*').eq('featured', true);
 
