bb4bb9eab9bd6fbba3ed6d5758c43b9efe5a3ee1
Daniel Gergely <daniel.gergely.2005@gmail.com>
2023-06-29 09:34:42 +0200

    css fix + random stuff

--- src/lib/components/Muhely.svelte
+++ src/lib/components/Muhely.svelte
@@ -2,7 +2,7 @@
 	import Muhely from '$lib/images/muhely.webp';
 </script>
 
-<div class="w-screen h-fit md:h-screen bg-light-orange border-black border-y-2 flex flex-col items-center px-4 py-24 mb-64" id="muhely">
+<div class="h-fit md:h-screen bg-light-orange border-black border-y-2 flex flex-col items-center px-4 py-24 mb-64" id="muhely">
 	<div class="bg-medium-orange px-3 pb-4 h-2 mt-4 mb-8 ml-4"><h2 class="text-black text-4xl font-semibold -mt-6 -ml-2">Műhely</h2></div>
 	<div class="w-full h-full flex flex-col-reverse lg:flex-row justify-center items-center md:px-10">
 		<img class="w-3/5 aspect-4/3 object-cover lg:w-1/3 border-black border-2 drop-shadow-xl-black-br mt-8 mx-10 lg:mx-15" src={Muhely} alt="A FRT műhelye" />
